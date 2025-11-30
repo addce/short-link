3b46318fa5b17fda1f6a2b0212d3c8ccb5315d22
addce3 <1779526338@qq.com>
2025-12-01 00:53:15 +0800

    c

--- deploy-scripts/install_dependencies.sh
+++ deploy-scripts/install_dependencies.sh
@@ -6,7 +6,7 @@ cd /home/ec2-user/short-link-backend/backend
 
 # 生产环境清理并重新安装（比 npm install 更可靠）
 rm -rf node_modules package-lock.json
-npm ci --only=production   # 只安装 dependencies，不装 devDependencies，体积小很多
+npm i   # 只安装 dependencies，不装 devDependencies，体积小很多
 
 # 给权限
 chmod -R 755 .
